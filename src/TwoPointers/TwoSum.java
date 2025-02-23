9bef45d09397920696104ce9c4b72353ca58d6b8
sonali-rajput <sonalirajput1088@gmail.com>
2025-02-23 07:13:22 +0000

    Add two sum sorted array

---
+++ src/TwoPointers/TwoSum.java
@@ -0,0 +1,35 @@
+package TwoPointers;
+
+import java.util.Arrays;
+
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+public class TwoSum {
+
+    public static void  main(String[] args){
+
+        int[] numbers = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(numbers,target)));
+    }
+    
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while(left<right) {
+            if ((numbers[left]+numbers[right]) > target) {
+                right = right +1;
+            }
+            else if (numbers[left]+numbers[right] < target) {
+                left = left +1;
+            }
+            else {
+                return new int[] {left+1,right+1};
+            }
+        }
+        return null;
+
+    }
+
+}
