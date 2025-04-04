d3b278fb9bbb379775180d7376bae26058ea65d9
sonali-rajput <sonalirajput1088@gmail.com>
2025-04-04 08:56:27 +0100

    Add buy and sell stock

---
+++ src/slidingWindow/BuyAndSellStock.java
@@ -0,0 +1,26 @@
+package slidingWindow;
+
+public class BuyAndSellStock {
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+
+        int min = prices[0];
+        int profit = 0;
+
+        for (int i=0;i<prices.length;i++) {
+            if(prices[i]<min) {
+                min = prices[i];
+            }
+            profit = Math.max(profit,prices[i]-min);
+        }
+
+        return profit;
+        
+    }
+
+}
